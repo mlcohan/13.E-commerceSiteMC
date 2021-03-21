9f062954185e04e5350b3d8c46aadf04372f91aa
Mary Cohan <mary.cohan@gmail.com>
2021-03-21 14:29:10 -0700

    update models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { canTreatArrayAsAnd } = require('sequelize/types/lib/utils');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,6 +8,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,14 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
-);
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key:'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key:'id'
+      }
+    },
+  });
 
 module.exports = ProductTag;
